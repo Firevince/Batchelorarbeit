d8b49f0402273723a784cd11c3a040ed612da1a3
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2023-10-30 12:40:19 +0100

    question similar docs

--- scripts/segment_ranking/rank_segments.py
+++ scripts/segment_ranking/rank_segments.py
@@ -1,6 +1,6 @@
-from scripts.db_connect import db_get_df, db_save_df
+from db_connect import db_get_df, db_save_df
 from scipy.spatial.distance import cosine
-from scripts.Embedding_creation.embedding_creator import dokument_embedding
+from Embedding_creation.embedding_creator import dokument_embedding
 import json
 
 
@@ -12,14 +12,15 @@ def calculate_document_question_distance(sentence_embedding,document_embedding):
     # print('Vector similarity for *different* meanings:  %.2f' % diff_bank)
     return diff_bank
 
-def get_5_most_similar_documents(message):
+def get_most_similar_documents(message, amount):
     # Initialisiere den DataFrame mit der Funktion aus db_init.py
-    df = db_get_df(["filename", "segment_text", "embedding_json"], table="transcripts_segments")
+    print("saved")
+    df = db_get_df(["filename", "segment_text", "embedding_json", "start", "end"], table="transcript_segments")
     question_embedding = dokument_embedding(message)
     df["distance"] = [calculate_document_question_distance(question_embedding, json.loads(document_embedding))for document_embedding in df["embedding_json"]]
-    most_similar_documents = df.nsmallest(5, "distance")
-
+    most_similar_documents = df.nsmallest(amount, "distance")
+    db_save_df(most_similar_documents, "best_fitting")
     return most_similar_documents
 
-user_input = "gulliver"
-db_save_df(get_5_most_similar_documents(user_input), "best_fitting")
+# user_input = "gulliver"
+# db_save_df(get_5_most_similar_documents(user_input), "best_fitting")
