e291353de8557f6d09ea035eb49b6a051504068e
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2023-10-30 12:27:58 +0100

    additional db functions

--- scripts/db_connect.py
+++ scripts/db_connect.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 import json
 
-database_path = "data/transcripts.sqlite"
+database_path = "/Users/br/Projects/Bachelorarbeit/data/transcripts.sqlite"
 
 def db_get_df(coloumns=["*"], table="transcript_segments"):
     con = sqlite3.connect(database_path)
@@ -15,7 +15,7 @@ def db_save_df(df, tablename):
         df.to_sql(tablename, con, index=False, if_exists='replace')
 
 
-def db_insert_row(data, filename):
+def db_insert_transcript_segment(data, filename):
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
 
@@ -44,7 +44,7 @@ def db_insert_row(data, filename):
     conn.commit()
     conn.close()
 
-def create_database():
+def create_table_transcript_segments():
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
     cursor.execute('''
@@ -61,5 +61,63 @@ def create_database():
     conn.commit()
     conn.close()
 
+def create_table_transcripts():
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS transcripts (
+            filename TEXT,
+            download_url TEXT,
+            audio_binary BLOB,
+            segment_count INTEGER
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
+def db_insert_transcript(data):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
 
+    cursor.execute('''
+        INSERT INTO transcripts (
+            filename,
+            download_url,
+            segment_count
+        ) VALUES (?, ?, ?)
+    ''', (
+        data['filename'],
+        data['download_url'],
+        data['segment_count']
+    ))
+
+    conn.commit()
+    conn.close()
+
+def db_insert_audio_binary(audio_file, filename):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        UPDATE transcripts SET audio_binary=?
+        where filename=?''', 
+        (audio_file, filename)
+    )
+
+    conn.commit()
+    conn.close()
+
+# 'https://media.neuland.br.de/file/2051900/c/feed/jonathan-swift-gullivers-reisen-2.mp3',
+# 'https://media.neuland.br.de/file/2052706/c/feed/weltweite-lieferketten-wer-verbindet-wer-haelt-wer-bedroht-sie.mp3',
+# 'https://media.neuland.br.de/file/1842747/c/feed/erben-und-vererben-was-steckt-hinter-dem-streit-ums-geld.mp3',
+# 'https://media.neuland.br.de/file/1574984/c/feed/sozialer-wohnungsbau-geschichte-eines-umstrittenen-konzepts.mp3',
+# 'https://media.neuland.br.de/file/1855770/c/feed/das-jahr-1000-als-die-globalisierung-begann-1.mp3',
+# 'https://media.neuland.br.de/file/2052397/c/feed/vater-mutter-kind-ist-die-kleinfamilie-am-ende.mp3',
+# 'https://media.neuland.br.de/file/1840258/c/feed/tierische-paar-ungs-geschichten-alles-natur.mp3',
+# 'https://media.neuland.br.de/file/1810432/c/feed/oriana-fallaci-kompromisslos-stolz-und-unbequem-1.mp3',
+# 'https://media.neuland.br.de/file/2046355/c/feed/ocker-die-edle-farbe-aus-der-erde-alles-natur.mp3',
+# 'https://media.neuland.br.de/file/32581/c/feed/das-muenchener-oktoberfest-brotzeit-bier-und-belustigung.mp3',
+# 'https://media.neuland.br.de/file/2051896/c/feed/die-legende-vom-sannikow-land-ein-paradies-im-eis.mp3',
+# 'https://media.neuland.br.de/file/2039883/c/feed/das-erste-maschinenzeitalter-von-der-kuriositaet-zur-revolution-1.mp3',
+# 'https://media.neuland.br.de/file/2041361/c/feed/autofiktion-wer-schreibt-hier-eigentlich.mp3',
+# 'https://media.neuland.br.de/file/2041360/c/feed/das-pseudonym-geheimnis-oder-verkaufsstrategie.mp3'
