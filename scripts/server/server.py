d160321b74c7f5ab6b6929f29a3d947424ad545e
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2023-12-12 16:40:14 +0100

    llama 2 embeddings

--- scripts/server/server.py
+++ scripts/server/server.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, request, send_from_directory
-from segment_ranking.rank_segments import get_most_similar_documents, get_most_similar_documents_tf_idf
+from segment_ranking.rank_segments import get_most_similar_documents_Bert, get_most_similar_documents_tf_idf, get_most_similar_documents_MINI_LM, get_most_similar_documents_Llama2
 from Audio_segmentation.split_audio import produce_snippets
 from Audio_segmentation.concat_audio import produce_audio
 
@@ -16,7 +16,7 @@ def process():
     user_input_time = int(request.form['time'])
 
     # Führe die gewünschten Funktionen aus
-    get_most_similar_documents_tf_idf(user_input_text, user_input_time)
+    get_most_similar_documents_Llama2(user_input_text, user_input_time)
     produce_snippets()
     produce_audio()
 
