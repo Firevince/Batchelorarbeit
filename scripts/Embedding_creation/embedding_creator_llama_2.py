37e7f6c4238a577ee41dcd0eccf845c9fd6f1c02
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2023-12-12 16:39:57 +0100

    llama 2 embeddings

--- scripts/Embedding_creation/embedding_creator_llama_2.py
+++ scripts/Embedding_creation/embedding_creator_llama_2.py
@@ -1,27 +1,20 @@
 from transformers import AutoModel, AutoTokenizer
 from sklearn.metrics.pairwise import cosine_similarity
+from db_connect import db_get_df, db_save_df
+import pandas as pd
+from datetime import datetime
 
-model = AutoModel.from_pretrained('mesolitica/llama2-embedding-1b-8k', trust_remote_code = True)
-tokenizer = AutoTokenizer.from_pretrained('mesolitica/llama2-embedding-1b-8k')
 
+def document_embedding_LLama_2(doc_text):
+    model = AutoModel.from_pretrained('mesolitica/llama2-embedding-1b-8k', trust_remote_code = True)
+    tokenizer = AutoTokenizer.from_pretrained('mesolitica/llama2-embedding-1b-8k')
+   
 
-input_ids = tokenizer(
-    [
-        'schreibt, hat mehr als ein narzisstisches Anliegen. Im eigenen Erleben spiegeln sich', 
-        'Isu perkauman: Kerajaan didakwa terdesak kaitkan pemimpin PN',
-        'nasi ayam tu sedap', 
-        'suka ikan goreng?',
-        'Kerajaan tidak akan berkompromi dengan isu perkauman dan agama yang dimanipulasi pihak tertentu untuk mengganggu-gugat kestabilan negara serta ketenteraman rakyat.',
-        'rasis bodo mamat tu',
-        'kerajaan sekarang xde otak',
-        'aku nak sukan olimpik ni',
-        'malaysia dapat x pingat kt sukan asia?',
-        'pingat gangsa menerusi terjun dan olahraga pada hari ke-10',
-        'Kerajaan negeri kini dibenarkan melaksanakan penerokaan awal unsur nadir bumi (REE) berdasarkan prosedur operasi standard (SOP) sedia ada untuk perlombongan nadir bumi dan mineral.',
-        'KONTINJEN Malaysia mendekati sasaran 27 pingat di Sukan Asia kali ini esok, selepas menuai dua lagi pingat gangsa menerusi terjun dan olahraga pada hari ke-10 pertandingan, pada Selasa.'
-    ], 
-    return_tensors = 'pt',
-    padding = True
-)
-v = model.encode(input_ids).detach().numpy()
-v.shape
+    input_ids = tokenizer(
+        doc_text, 
+        return_tensors = 'pt',
+        padding = True
+    )
+    doc_embedding = model.encode(input_ids).detach().numpy()[0]
+    return doc_embedding
+
