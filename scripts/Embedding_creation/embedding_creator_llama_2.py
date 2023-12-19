22c3edea2ee43b751d85f98f260cc9734e9c1d6e
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2023-12-19 12:36:34 +0100

    llama embeddings on kiz

--- scripts/Embedding_creation/embedding_creator_llama_2.py
+++ scripts/Embedding_creation/embedding_creator_llama_2.py
@@ -1,27 +1,24 @@
 from transformers import AutoModel, AutoTokenizer
 from sklearn.metrics.pairwise import cosine_similarity
+from db_connect import db_get_df, db_save_df
+import pandas as pd
+from datetime import datetime
+
 
 model = AutoModel.from_pretrained('mesolitica/llama2-embedding-1b-8k', trust_remote_code = True)
 tokenizer = AutoTokenizer.from_pretrained('mesolitica/llama2-embedding-1b-8k')
 
+df = db_get_df(["*"], "transcript_segments")
 
 input_ids = tokenizer(
-    [
-        'schreibt, hat mehr als ein narzisstisches Anliegen. Im eigenen Erleben spiegeln sich', 
-        'Isu perkauman: Kerajaan didakwa terdesak kaitkan pemimpin PN',
-        'nasi ayam tu sedap', 
-        'suka ikan goreng?',
-        'Kerajaan tidak akan berkompromi dengan isu perkauman dan agama yang dimanipulasi pihak tertentu untuk mengganggu-gugat kestabilan negara serta ketenteraman rakyat.',
-        'rasis bodo mamat tu',
-        'kerajaan sekarang xde otak',
-        'aku nak sukan olimpik ni',
-        'malaysia dapat x pingat kt sukan asia?',
-        'pingat gangsa menerusi terjun dan olahraga pada hari ke-10',
-        'Kerajaan negeri kini dibenarkan melaksanakan penerokaan awal unsur nadir bumi (REE) berdasarkan prosedur operasi standard (SOP) sedia ada untuk perlombongan nadir bumi dan mineral.',
-        'KONTINJEN Malaysia mendekati sasaran 27 pingat di Sukan Asia kali ini esok, selepas menuai dua lagi pingat gangsa menerusi terjun dan olahraga pada hari ke-10 pertandingan, pada Selasa.'
-    ], 
+    df["segment_text"][1500:1800].to_list(), 
     return_tensors = 'pt',
     padding = True
-)
+) 
+print(datetime.now(), " starting embedding calculation")
 v = model.encode(input_ids).detach().numpy()
-v.shape
+print(datetime.now(), " ended embedding calculation")
+
+df = pd.DataFrame(v)
+db_save_df(df, "transcript_segments_llama_2_y")
+
