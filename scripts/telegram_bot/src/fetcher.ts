8166e769386fda9deaf1bba63db628a4a9cc46c6
Florian Thoma <flori.thoma@gmail.com>
2024-02-06 20:24:16 +0100

    added telegraf initial bot

---
+++ scripts/telegram_bot/src/fetcher.ts
@@ -0,0 +1,3 @@
+export const fetchUrlBySearchQuery = async (query: string) => {
+	return "https://animaapp.s3.amazonaws.com/sample-files/audio/pixabay-free-service-bell-ring-14610.mp3"
+}
--- scripts/telegram_bot/src/index.ts
+++ scripts/telegram_bot/src/index.ts
@@ -1,7 +1,42 @@
+import { Context, Telegraf } from 'telegraf'
+import { Update } from 'telegraf/typings/core/types/typegram';
+import { fetchUrlBySearchQuery } from './fetcher';
+
+const BOT_TOKEN = "6676938301:AAHmBBV9d809u3TiWD3DorGk1QK4S1JoPJU";
 
 
-const start = () => {
-	console.log("Hello");
+const bot = new Telegraf(BOT_TOKEN);
+
+const startBot = async () => {
+	console.log("Bot running");
+	bot.use((ctx) => {		
+		const postJson = ctx.channelPost;
+		const text = (postJson as any)?.text || "";
+		if(text){ onNextText(text, ctx);}
+	})
+    
+    await bot.launch(); //start the bot
+
+    // Handling shutdowns gracefully
+    process.once('SIGINT', () => bot.stop('SIGINT'));
+    process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+	console.log("Bot finished");
 }
 
-start();
+const onNextText = async (text: string, ctx: Context<Update>) => {
+	const audioUrl = await fetchUrlBySearchQuery(text);
+    const title = text;
+    ctx.replyWithAudio(
+        { url: audioUrl },
+        { 
+            title: "Dein Podcast",
+            caption: title
+        }
+    );
+	ctx.reply('Welcome ' + audioUrl)
+}
+
+startBot();
+
+
