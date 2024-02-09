1a34f15c4a13e42c65e2a857130eb18ae64b4ee9
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2024-02-09 11:13:16 +0100

    removed static paths

--- scripts/Audio_segmentation/concat_audio.py
+++ scripts/Audio_segmentation/concat_audio.py
@@ -1,6 +1,11 @@
 import os
 
+from dotenv import load_dotenv
 from pydub import AudioSegment
+
+load_dotenv()
+DATA_PATH = os.getenv("DATA_PATH")
+SERVER_PATH = os.getenv("SERVER_PATH")
 
 
 def concatenate_segments_with_sound(segment_files, output_file, inbetween_sound_file):
@@ -17,9 +22,9 @@ def concatenate_segments_with_sound(segment_files, output_file, inbetween_sound_
 
 def produce_final_audio():
     segment_files = []
-    output_file = "/Users/br/Projects/Bachelorarbeit/scripts/server/static/audio/concatenated_audio.mp3"
-    inbetween_sound_file = "/Users/br/Projects/Bachelorarbeit/data/inter.wav"
-    snippets_path = "/Users/br/Projects/Bachelorarbeit/data/audio_segments/"
+    output_file = os.path.join(SERVER_PATH, "static/audio/concatenated_audio.mp3")
+    inbetween_sound_file = os.path.join(DATA_PATH, "inter.wav")
+    snippets_path = os.path.join(DATA_PATH, "audio_segments/")
 
     for filename in sorted(os.listdir(snippets_path)):
         segment_files.append(snippets_path + filename)
