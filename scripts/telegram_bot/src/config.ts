75f90d27674c04008a00c5bad46b46570f15f3a3
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2024-02-09 11:15:19 +0100

    changed telegram url to localhost

--- scripts/telegram_bot/src/config.ts
+++ scripts/telegram_bot/src/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-	BASE_URL: "https://2339-2001-9e8-3d1f-5300-1dd2-1959-a12e-ac5f.ngrok-free.app",
 	BOT_TOKEN: "6676938301:AAHmBBV9d809u3TiWD3DorGk1QK4S1JoPJU",
+	BASE_URL: "http://localhost:8000"
 }
