e65bbd6725b055d18233ed602a22a70a9458810f
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2024-02-14 15:43:43 +0100

    refactored

--- scripts/server/app.py
+++ scripts/server/app.py
@@ -1,6 +1,8 @@
+import html
 import json
 import os
 
+from audio_downloader.episodes_downloader import download_on_demand
 from audio_segmentation.concat_audio import produce_final_audio
 from audio_segmentation.get_audio_metadata import save_all_images
 from audio_segmentation.split_audio import produce_audio_snippets
@@ -31,30 +33,31 @@ def process_web_get():
 
 
 def process_web(user_input_text, user_input_time):
-    print(f"processing '{user_input_text}' for {user_input_time} minutes")
 
-    df_documents = get_most_similar_segments("TF_IDF", user_input_text, user_input_time, 3)
+    df_documents, out_filename = produce_audio(user_input_text, user_input_time)
 
-    df_documents = save_all_images(df_documents)
-    produce_audio_snippets(df_documents)
-    produce_final_audio()
-
+    save_all_images(df_documents)
     keywords = gpt_get_keywords(df_documents)
-
     rows = [row[1] for row in df_documents.iterrows()]
 
     return render_template("index.html", rows=rows, keywords=keywords)
+
+
+def produce_audio(user_query, time):
+    print(f"processing '{user_query}' for {time} minutes")
+    documents = get_most_similar_segments("TF_IDF", user_query, time, 5)
+    produce_audio_snippets(documents)
+    out_filename = html.escape(user_query)
+    produce_final_audio(out_filename)
+    return (documents, out_filename)
 
 
 @app.route("/api")
 def api():
     user_input_text = request.args.get("text", default="", type=str)
     user_input_time = int(request.args.get("time", default=5))
-    print(f"processing '{user_input_text}' for {user_input_time} minutes")
-    documents = get_most_similar_segments("TF_IDF_MINI_LM", user_input_text, user_input_time, 5)
-    produce_audio_snippets(documents)
-    produce_final_audio()
-    return_dict = {"url": "/static/audio/concatenated_audio.mp3"}
+    _, out_filename = produce_audio(user_input_text, user_input_time)
+    return_dict = {"url": os.path.join("/static/audio", out_filename)}
     return jsonify(return_dict)
 
 
