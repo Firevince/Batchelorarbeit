573adc11f79d0a593a459422199cdad597f62185
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2024-02-14 15:45:51 +0100

    support for lemmatisation pipeline

--- scripts/Embedding_creation/embedding_creator_TF_IDF.py
+++ scripts/Embedding_creation/embedding_creator_TF_IDF.py
@@ -4,10 +4,15 @@ import joblib
 import numpy as np
 import spacy
 from db_connect import db_get_df, load_npz, load_pkl, save_npz, save_pkl
+from dotenv import load_dotenv
+from german_compound_splitter import comp_split
 from scipy import sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import pairwise_distances
 from tqdm import tqdm
+
+load_dotenv()
+DATA_PATH = os.getenv("DATA_PATH")
 
 
 def calc_all_tf_idf():
@@ -21,6 +26,50 @@ def calc_all_tf_idf():
     matrix_name = f"tf_idf_matrix{vocab_len//1000}k.npz"
     save_pkl(tfidf_vectorizer, vectorizer_name)
     save_npz(tfidf_matrix, matrix_name)
+
+
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
+def compound_split_sentence(sentence, ahocs):
+    compound_split_sentence_words = []
+    for word in sentence.split():
+        if is_number(word):
+            continue
+        try:
+            dissection = comp_split.dissect(word, ahocs, make_singular=True)
+        except:
+            dissection = ""
+        for split_word in dissection:
+            compound_split_sentence_words.append(split_word)
+    return " ".join(compound_split_sentence_words)
+
+
+def lemmatize_german_sentence(input_sentence, nlp):
+    doc = nlp(input_sentence)
+    lemmatized_words = []
+    for token in doc:
+        lemmatized_words.append(token.lemma_)
+    lemmatize_sentence = " ".join(lemmatized_words)
+    return lemmatize_sentence
+
+
+def question_embedding_tf_idf_lemma_compound_split(question):
+    input_file = os.path.join(DATA_PATH, "german.dic")
+    ahocs = comp_split.read_dictionary_from_file(input_file)
+    nlp = spacy.load("de_core_news_md")
+    tfidf_vectorizer = load_pkl("tfidf_vectorizer_compound_split_87k.pkl")
+
+    question = lemmatize_german_sentence(question, nlp)
+    question = compound_split_sentence(question, ahocs)
+    question_embedding = tfidf_vectorizer.transform([question])
+
+    return question_embedding
 
 
 ## old code
@@ -69,9 +118,7 @@ def get_idf_from_doc(document):
     message_lemmatized = lemmatize_german_sentence(document, nlp)
 
     encoded_words = [
-        (idf_dict[word.lower()], word)
-        for word in message_lemmatized
-        if word.lower() in idf_dict
+        (idf_dict[word.lower()], word) for word in message_lemmatized if word.lower() in idf_dict
     ]
     return encoded_words
 
@@ -100,3 +147,5 @@ def calculate_distances_custom(message):
 # print(len(tfidf_vectorizer.get_vocab()))
 
 # TODO try save as multiindexed or seperate table
+
+# question_embedding_tf_idf_lemma_compound_split("Was ist der Capital Bra")
