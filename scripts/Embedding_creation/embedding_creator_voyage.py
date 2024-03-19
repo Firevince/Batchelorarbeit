1de82ffc0c28199ee3e1775f1245292d13b84993
neumannvi84434 <neumannvi84434@th-nuernberg.de>
2024-03-19 17:41:05 +0100

    evaluation+voyage

--- scripts/Embedding_creation/embedding_creator_voyage.py
+++ scripts/Embedding_creation/embedding_creator_voyage.py
@@ -16,12 +16,5 @@ vo = voyageai.Client()
 def get_embedding_voyage(input, model="voyage-2"):
     if isinstance(input, str):
         input = [input.replace("\n", " ")]
-
-    # request =  {
-    #     "input": json.dumps(input),
-
-    # }
-    # input = json.dumps(input)
-
     result = vo.embed(input, model=model, input_type="document")
     return result.embeddings
